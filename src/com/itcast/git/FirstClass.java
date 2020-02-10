0d76fb6299ea6b610d43590cf9abb9b8b875d400
LoveSunny <656854964@qq.com>
2020-02-10 10:27:59 +0800

    第一次提交

--- src/com/itcast/git/FirstClass.java
+++ src/com/itcast/git/FirstClass.java
@@ -4,4 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello gitClass");
     }
+    public void test(){
+        System.out.println("这是一个test");
+    }
 }
