e3a0bbd3020bf58b90440af893c42dd73e628911
LoveSunny <656854964@qq.com>
2020-02-10 10:43:41 +0800

    修改firstclass

--- src/com/itcast/git/FirstClass.java
+++ src/com/itcast/git/FirstClass.java
@@ -3,8 +3,12 @@ package com.itcast.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello gitClass");
+//        hello test
     }
     public void test(){
+        int i = 0;
+        ++i;
+        System.out.println(i);
         System.out.println("这是一个test");
     }
 }
